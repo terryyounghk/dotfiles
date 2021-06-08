26e3afe73352d57d663cb425a03d8248c3e97e0a
Terry Young <terryyounghk@gmail.com>
2021-06-08 14:27:28 +0800

    add homebrew installation and brewfile

---
+++ homebrew/brew.sh
@@ -0,0 +1,28 @@
+#!/bin/sh
+
+# routine
+[ ! -f script/helper ] && echo "Please run this from $HOME/.dotfiles" && exit 1
+. script/helper
+
+set -e
+
+# Install the correct homebrew for each OS type
+if test "$(uname)" = "Darwin"
+then
+  # Check for Homebrew
+  if test ! $(which brew)
+  then
+    echo "  Installing Homebrew for you."
+    ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+  fi
+
+  # Upgrade homebrew
+  echo "› brew update"
+  brew update
+
+  # Run Homebrew through the Brewfile
+  cd homebrew
+  echo "› brew bundle"
+  brew bundle
+  cd ..
+fi
