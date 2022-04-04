96992a814da15e98f45e3e15e56289975065a6a0
Terry Young <terryyounghk@gmail.com>
2022-04-04 23:38:33 +0800

    update nvm

--- nvm/install.sh
+++ nvm/install.sh
@@ -5,16 +5,28 @@
 . script/helper
 
 echo ''
-echo 'Installing NodeJS with NVM (Uninstall NodeJS installed from pkg first)...'
+echo 'Installing nvm...'
 echo ''
-sudo ./nvm/uninstall-node.sh
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.1/install.sh | bash
+
+#source nvm/nvm.symlink/nvm.sh
 
 echo ''
-echo 'Installing NodeJS with NVM...'
+echo 'Installing latest npm and nodejs lts...'
 echo ''
-source nvm/nvm.symlink/nvm.sh
-
 nvm install --latest-npm --lts
 nvm alias default stable
+
+# follow the message's instructions in case npm needs upgrading
+
+# yarn
+curl -o- -L https://yarnpkg.com/install.sh | bash
+
+# global packages
+npm install -g \
+  eslint \
+  ts-node \
+  typescript \
+  yarn
 
 exit 0
--- zim/custom/nvm/init.zsh
+++ zim/custom/nvm/init.zsh
@@ -1,6 +1,18 @@
+# Caution if your nvm dir or home dir is symbolic link
 # NVM - load NVM without causing issues with "npm config prefix"
 # See: https://stackoverflow.com/a/58559982/277666
-export NVM_DIR="$HOME/.dotfiles/nvm/nvm.symlink"
-[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh" # This loads NVM
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
 [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
 
+nvm-install () {
+  # yarn
+  curl -o- -L https://yarnpkg.com/install.sh | bash
+
+  # global packages
+  npm install -g \
+    eslint \
+    ts-node \
+    typescript \
+    yarn
+}
