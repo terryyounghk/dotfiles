6fa3608f28a4a376b74bc6a8c40160dbe0f725a4
Terry Young <terryyounghk@gmail.com>
2022-04-05 00:01:48 +0800

    update rime

--- rime/install.sh
+++ rime/install.sh
@@ -5,6 +5,25 @@
 . script/helper
 
 # https://rime.im/
-# https://github.com/rime/plum
 
-rime/plum/rime-install prelude essay jyutping quick scj emoji
+# Rime should already be installed: brew install --cask squirrel
+# See: https://formulae.brew.sh/cask/squirrel
+
+# now use plum to install various input methods
+
+# https://github.com/rime/plum
+curl -fsSL https://git.io/rime-install | bash
+
+# install additional input methods
+
+plum/rime-install jyutping rime-cantonese
+
+# clean up
+
+rm -rf ./plum
+
+# copy custom configurations
+
+cp ./default.custom.yaml ~/Library/Rime/
+
+# first time usage: activate the input method and choose "deploy" from the menu
