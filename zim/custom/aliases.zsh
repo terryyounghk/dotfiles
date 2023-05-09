d1f1d5f954a803788d5c1348c8727e43104a30e8
Terry Young <terryyounghk@gmail.com>
2023-05-09 13:30:54 +0800

    add zsh-hist

--- zim/custom/aliases.zsh
+++ zim/custom/aliases.zsh
@@ -135,6 +135,14 @@ history-grep () {
 # -----------------------------------------------------------------------------------
 # hist, see https://github.com/marlonrichert/zsh-hist
 
+alias h=hi
+
+# [h]ist [a]ll
+alias ha="hist l $'*'"
+
+# [h]ist [t]en entries
+alias ht='hist l {-1..-10}'
+
 alias hh='hist help'
 alias hf='hist f'
 alias hfl='hist f -1'
@@ -145,6 +153,68 @@ alias hg='hist g'
 alias hgl='hist g -1'
 alias hd='hist d'
 alias hdl='hist d -1'
+
+# interactive with fzf
+
+# [h]ist [i]nteractive
+hi () {
+  local cmd=$1
+  local sel
+  local items
+  local choice
+
+  case $cmd in
+    "copy" | "c" | "compress" | "d" | "delete" | "e" | "edit" | "f" | "fix" | "g" | "get" | "l" | "list" | "n" | "normalize")
+      ;;
+    *)
+      items=("1. compress" "2. delete" "3. edit" "4. fix" "5. get" "6. list" "7. normalize" "8. copy")
+      cmd=$(printf "%s\n" "${items[@]}" | fzf --header="Choose a hist command:" | ifne cut -d ' ' -f 2-)
+      ;;
+  esac
+
+  if [ "$cmd" = "" ]; then
+    echo "No command selected. Default to 'get'."
+    cmd=get
+  fi
+
+  echo "Selected command: $cmd"
+
+  # ------------------------------------------------------------------------------------------------------------------------------------------------------------
+  #     list all from hist
+  #     |             remove color codes
+  #     |             |                                  sort reversed
+  #     |             |                                  |         multi select
+  #     |             |                                  |         |        trim leading/trailing spaces from each line
+  #     |             |                                  |         |        |                 replace multiple spaces with one
+  #     |             |                                  |         |        |                 |           cut first column to get only the entry numbers
+  #     |             |                                  |         |        |                 |           |                 join lines with space as separator
+  #     |             |                                  |         |        |                 |           |                 |
+  sel=$(hist l $'*' | sed "s,\x1B\[[0-9;]*[a-zA-Z],,g" | sort -r | fzf -m | awk '{$1=$1};1' | tr -s ' ' | cut -d ' ' -f 1 | tr '\n' ' ')
+
+  if [ "$sel" = "" ]; then
+    echo "No selection from hist. Abort."
+    return 1
+  fi
+
+  echo "Selected hist entries: $sel"
+
+  case $cmd in
+    "copy")
+      # ------------------------------------------------------------------------------------
+      #                                           get everything after the first space
+      #                                           |                  copy to clipboard
+      #                                           |                  |
+      hist g $(echo $sel) | awk '{$1=$1};1' | tr -s ' ' | cut -d ' ' -f 2- | pbcopy
+      echo "The following commands are copied to clipboard:"
+      hist l $(echo $sel)
+      ;;
+    *)
+      # echo hist $cmd $(echo $sel)
+      # don't know why $sel need echo but it works for now (macos)
+      hist $cmd $(echo $sel)
+      ;;
+  esac
+}
 
 # -----------------------------------------------------------------------------------
 # git
