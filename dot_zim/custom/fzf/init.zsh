b79c394efbb73edf5eb76d85f5290e1fccb8bb32
Terry Young <terryyounghk@gmail.com>
2024-09-07 01:08:19 +0800

    update: FZF_DEFAULT_OPTS
    - make it bigger if tmux
    - move dracula-theme portion to a variable

--- dot_zim/custom/fzf/init.zsh
+++ dot_zim/custom/fzf/init.zsh
@@ -2,6 +2,8 @@
 
 # dracula theme: https://draculatheme.com/fzf
 
-export FZF_DEFAULT_OPTS="--tmux 70% --layout=reverse --inline-info --color=fg:#f8f8f2,bg:#282a36,hl:#bd93f9 --color=fg+:#f8f8f2,bg+:#44475a,hl+:#bd93f9 --color=info:#ffb86c,prompt:#50fa7b,pointer:#ff79c6 --color=marker:#ff79c6,spinner:#ffb86c,header:#6272a4"
+FZF_DRACULA_THEME="--color=fg:#f8f8f2,bg:#282a36,hl:#bd93f9 --color=fg+:#f8f8f2,bg+:#44475a,hl+:#bd93f9 --color=info:#ffb86c,prompt:#50fa7b,pointer:#ff79c6 --color=marker:#ff79c6,spinner:#ffb86c,header:#6272a4"
+
+export FZF_DEFAULT_OPTS="--tmux 90% --layout=reverse --inline-info ${FZF_DRACULA_THEME}"
 
 # export FZF_CTRL_T_COMMAND="command fd -H --no-ignore-vc-E .git -td -tf"
