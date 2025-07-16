8977736657618d9aa1a97e1ed774882bb8ea6fe3
Terry Young <terryyounghk@gmail.com>
2025-07-16 21:55:43 +0800

    fix: nvim copilot nodejs < 20 issue

---
+++ private_dot_config/nvim/lua/plugins/copilot.lua
@@ -0,0 +1,11 @@
+-- https://github.com/zbirenbaum/copilot.lua
+
+return {
+  "zbirenbaum/copilot.lua",
+  opts = {
+    server = {
+      -- https://github.com/zbirenbaum/copilot.lua/discussions/468
+      type = 'binary',
+    },
+  }
+}
