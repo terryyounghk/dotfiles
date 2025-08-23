2dee3577ae53524ca21eb1a1e0bcc38bd9ac2e98
Terry Young <terryyounghk@gmail.com>
2025-08-24 04:47:01 +0800

    fix: fully use atuin default bindings and remove conflicts

--- dot_zim/custom/atuin/init.zsh
+++ dot_zim/custom/atuin/init.zsh
@@ -1,12 +1,11 @@
+# https://atuin.sh/
 # Use atuin to power ctrl-r history search but with fzf.
 # Also disable atuin's up arrow bindings and use alt-r to bring up atuin's own tui
 # prerequisites: brew install coreutils atuin
 
 atuin-setup() {
   ! hash atuin && return
-  bindkey '\er' _atuin_search_widget
 
-  export ATUIN_NOBIND="true"
   eval "$(atuin init zsh)"
   fzf-atuin-history-widget() {
     local selected num
@@ -22,7 +21,7 @@ atuin-setup() {
     return $ret
   }
   zle -N fzf-atuin-history-widget
-  bindkey '^R' fzf-atuin-history-widget
+  bindkey '\er' fzf-atuin-history-widget
 }
 
 atuin-setup
